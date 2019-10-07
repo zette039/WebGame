a8aa6167227b55042a3da702a8daec04769df57e
Zachary Steven Zettervall <zette039@umn.edu>
2019-10-07 14:51:29 -0500

    Automated update script

--- js/game.js
+++ js/game.js
@@ -1,30 +1,43 @@
-var enemies;
+var enemies; //(x,y) has the max (from 1,1) of (1600, 1900)
 var player;
 var myScore;
+var score = new component(0,0,0,0,0,0,1)
+
 function startGame() {
-    myGameArea.start();
-    player = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
-    enemies = new component(90, 49.6, "images/enemy.png", 1000, 20, 'image');
-     myScore = new component("20px", "Consolas", "black", 0, 40, "text");
-    score.number = 0;
+ myGame.resize();
+ myGame.start();
+ player = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
+ enemies = new component(90, 49.6, "images/enemy.png", 1000, 20, 'image');
+ myScore = new component("20px", "Consolas", "black", 0, 40, "text");
+ score.number = 0;
 }
 
-var myGameArea = {
+var myGame = {
     canvas : document.createElement("canvas"),
+    resize : function() {
+        if (window.innerWidth*16>window.innerHeight*9) {
+         this.canvas.width = Math.min(window.innerWidth, 16*100);
+         this.canvas.height = 9/16*Math.min(window.innerWidth, 16*100);
+        } else {
+         this.canvas.width = 16/9*Math.min(window.innerHeight, 9*100); //600;
+         this.canvas.height = Math.min(window.innerHeight, 9*100); //300;
+        }
+    },
     start : function() {
-        this.canvas.width = 600;
-        this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = (e.type == "keydown");
+            myGame.keys = (myGame.keys || []);
+            myGame.keys[e.keyCode] = (e.type == "keydown");
         })
         window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = (e.type == "keydown");            
+            myGame.keys[e.keyCode] = (e.type == "keydown");
         })
-    }, 
+        window.addEventListener('resize', function (e) {
+            myGame.resize()
+        })
+    },
     clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
@@ -32,23 +45,22 @@ var myGameArea = {
     clearInterval(this.interval);
   }
 }
-var score = new component(0,0,0,0,0,0,1)
 function component(width, height, color, x, y, type, number) {
  this.type = type;
  if (type == "image") {
     this.image = new Image();
     this.image.src = color;
   }
-    this.gamearea = myGameArea;
+    this.gamearea = myGame;
     this.number = number + 1;
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
-    this.y = y;    
+    this.y = y;
     this.update = function() {
-        ctx = myGameArea.context;
+        ctx = myGame.context;
          if (type == "image") {
       ctx.drawImage(this.image,
         this.x,
@@ -66,7 +78,7 @@ function component(width, height, color, x, y, type, number) {
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;        
-    }  
+    }
      this.crashWith = function(otherobj) {
     var myleft = this.x;
     var myright = this.x + (this.width);
@@ -92,18 +104,17 @@ function component(width, height, color, x, y, type, number) {
 
 function updateGameArea() {
 if (player.crashWith(enemies)) {
-    myGameArea.stop();
+    myGame.stop();
   } else {
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
+    myGame.clear();
+    myGame.frameNo += 1;
     player.speedX = 0;
     player.speedY = 0;  
- 
-    
-    if (myGameArea.keys && myGameArea.keys[37] && player.x > 10) {player.speedX = -5;}
-    if (myGameArea.keys && myGameArea.keys[39] && player.x < 370) {player.speedX = 5; }
-    if (myGameArea.keys && myGameArea.keys[38] && player.y > 10) {player.speedY = -5; }
-    if (myGameArea.keys && myGameArea.keys[40] && player.y < 250.4) {player.speedY = 5; }
+
+    if (myGame.keys && myGame.keys[37] && player.x > 10) {player.speedX = -5;}
+    if (myGame.keys && myGame.keys[39] && player.x < 370) {player.speedX = 5; }
+    if (myGame.keys && myGame.keys[38] && player.y > 10) {player.speedY = -5; }
+    if (myGame.keys && myGame.keys[40] && player.y < 250.4) {player.speedY = 5; }
     player.newPos();    
     player.update();
       myScore.text = "SCORE: " + score.number;
