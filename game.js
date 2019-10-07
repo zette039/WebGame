2fd4b99b8f20d4e8c681c1f067fd8a0ec976d414
zette039 <zette039@umn.edu>
2019-10-06 20:13:45 -0400

    Automated update script

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var player;
 var myScore;
 function startGame() {
     myGameArea.start();
-    player = new component(20, 20, 'car.png', 10, 90, 'image');
+    player = new component(20, 20, 'images/car.png', 10, 90, 'image');
     enemies = new component(20, 60, "green", 370, 20);
      myScore = new component("20px", "Consolas", "black", 200, 40, "text");
     score.number = 0;
