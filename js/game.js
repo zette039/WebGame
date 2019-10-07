81b881677bcaab2672633227221b89e7aeab0579
chadurvig <56233767+chadurvig@users.noreply.github.com>
2019-10-07 16:58:01 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,154 +1,181 @@
-var enemies; //(x,y) has the max (from 1,1) of (1600, 1900)
+var enemy1 = false;
+var enemy2 = false;
+var enemy3 = false;
+var enemy4 = false;
+function et1 (){
+enemy1 = true;
+}
+
+function et2 (){
+enemy1 = true;
+enemy2 = true;
+}
+
+function et3 (){
+enemy1 = true;
+enemy2 = true;
+enemy3 = true;
+}
+
+function et4 (){
+enemy1 = true;
+enemy2 = true;
+enemy3 = true;
+enemy4 = true;
+}
+
 var player;
 var myScore;
-var score = new component(0,0,0,0,0,0,1)
-
 function startGame() {
- myGame.resize();
- myGame.start();
- player = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
- enemies = new component(90, 49.6, "images/enemy.png", 1000, 20, 'image');
- myScore = new component("20px", "Consolas", "black", 0, 40, "text");
- score.number = 0;
+    myGameArea.start();
+    player = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
+    e1 = new component(90, 49.6, "images/enemy.png", 1000, 20, 'image');
+    e2 = new component(90, 49.6, "images/enemy.png", 1000, 100, 'image');
+    e3 = new component(90, 49.6, "images/enemy.png", 1000, 200, 'image');
+    e4 = new component(90, 49.6, "images/enemy.png", 1000, 250.4, 'image');
+     myScore = new component("20px", "Consolas", "black", 0, 40, "text");
+    score.number = 0;
 }
 
-var myGame = {
- canvas : document.createElement("canvas"),
- getSize : function() {
-  if (window.innerWidth*9<window.innerHeight*16) {
-   getSizeWidth = Math.min(window.innerWidth, 16*100);
-   getSizeHeight = 9/16*Math.min(window.innerWidth, 16*100);
-  } else {
-   getSizeWidth = 16/9*Math.min(window.innerHeight, 9*100); //600;
-   getSizeHeight = Math.min(window.innerHeight, 9*100); //300;
-   }
-  return [getSizeWidth-10, getSizeHeight-10] //-10 for boarders
- },
- resize : function() {
-    //if (window.innerWidth*16>window.innerHeight*9) {
-    // this.canvas.width = Math.min(window.innerWidth, 16*100);
-   ///  this.canvas.height = 9/16*Math.min(window.innerWidth, 16*100);
-  ///  } else {
-    // this.canvas.width = 16/9*Math.min(window.innerHeight, 9*100); //600;
-    // this.canvas.height = Math.min(window.innerHeight, 9*100); //300;
-   // }
-   [this.canvas.width, this.canvas.height] = myGame.getSize()
-  },
-  start : function() {
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-    window.addEventListener('keydown', function (e) {
-      myGame.keys = (myGame.keys || []);
-      myGame.keys[e.keyCode] = (e.type == "keydown");
-    })
-    window.addEventListener('keyup', function (e) {
-      myGame.keys[e.keyCode] = (e.type == "keydown");
-    })
-    window.addEventListener('resize', function (e) {
-      myGame.resize()
-    })
-  },
-  clear : function(){
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
- stop : function() {
-  clearInterval(this.interval);
- }
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 600;
+        this.canvas.height = 300;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");            
+        })
+    }, 
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+  stop : function() {
+    clearInterval(this.interval);
+  }
 }
+var score = new component(0,0,0,0,0,0,1)
 function component(width, height, color, x, y, type, number) {
  this.type = type;
  if (type == "image") {
-  this.image = new Image();
-  this.image.src = color;
- }
-  this.gamearea = myGame;
-  this.number = number + 1;
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGame.context;
-     if (type == "image") {
-   ctx.drawImage(this.image,
-    this.x,
-    this.y,
-    this.width, this.height);
-  } else {
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);}
-    if (this.type == "text") {
-   ctx.font = this.width + " " + this.height;
-   ctx.fillStyle = color;
-   ctx.fillText(this.text, this.x, this.y);
-  } else {
-  }}
-  this.newPos = function() {
-    this.x += this.speedX;
-    this.y += this.speedY;    
+    this.image = new Image();
+    this.image.src = color;
   }
-   this.crashWith = function(otherobj) {
-  var myleft = this.x;
-  var myright = this.x + (this.width);
-  var mytop = this.y;
-  var mybottom = this.y + (this.height);
-  var otherleft = otherobj.x;
-  var otherright = otherobj.x + (otherobj.width);
-  var othertop = otherobj.y;
-  var otherbottom = otherobj.y + (otherobj.height);
-  var crash = true;
-  if ((mybottom < othertop) ||
-  (mytop > otherbottom) ||
-  (myright < otherleft) ||
-  (myleft > otherright)) {
-   crash = false;
+    this.gamearea = myGameArea;
+    this.number = number + 1;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+         if (type == "image") {
+      ctx.drawImage(this.image,
+        this.x,
+        this.y,
+        this.width, this.height);
+    } else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);}
+        if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    } else {
+    }}
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }  
+     this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+    (mytop > otherbottom) ||
+    (myright < otherleft) ||
+    (myleft > otherright)) {
+      crash = false;
+    }
+    return crash;
   }
-  return crash;
- }
 }
  
 
 
 
 function updateGameArea() {
- if (player.crashWith(enemies)) {
-  myGame.stop();
- } else {
-  myGame.clear();
-  myGame.frameNo += 1;
-  player.speedX = 0;
-  player.speedY = 0; 
-  if (myGame.keys && myGame.keys[37] && player.x > 10) {player.speedX = -5;}
-  if (myGame.keys && myGame.keys[39] && player.x < 370) {player.speedX = 5; }
-  if (myGame.keys && myGame.keys[38] && player.y > 10) {player.speedY = -5; }
-  if (myGame.keys && myGame.keys[40] && player.y < 250.4) {player.speedY = 5; }
-  player.newPos();  
-  player.update();
-  myScore.text = "SCORE: " + score.number;
-  score.update();
+if (player.crashWith(enemies)) {
+    myGameArea.stop();
+  } else {
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    player.speedX = 0;
+    player.speedY = 0;  
+ 
+    
+    if (myGameArea.keys && myGameArea.keys[37] && player.x > 10) {player.speedX = -5;}
+    if (myGameArea.keys && myGameArea.keys[39] && player.x < 370) {player.speedX = 5; }
+    if (myGameArea.keys && myGameArea.keys[38] && player.y > 10) {player.speedY = -5; }
+    if (myGameArea.keys && myGameArea.keys[40] && player.y < 250.4) {player.speedY = 5; }
+    player.newPos();    
+    player.update();
+      myScore.text = "SCORE: " + score.number;
+      score.update();
   myScore.update();
-  enemies.newPos();
-  enemies.update();
-  enemies.x += -15;
-  if(enemies.x <= -49.6){
-   score.number += 1;
-   enemies.x = 1000;
-   enemies.y = Math.floor(Math.random() * 250.4) + 1
-  }
- }
-}
-
-
-
-
-
-
-
-
-
-
-
-
+   if(enemy1 == true){
+    e1.update();
+    e1.x += -15;
+    }
+    
+    if(enemy2 == true){
+    e2.update();
+    e2.x += -15;
+    }
+    
+    if(enemy3 == true){
+    e3.update();
+    e3.x += -15;
+    }
+   
+   if(enemy4 == true){
+    e4.update();
+    e4.x += -15;
+    }
+   
+    if(e1.x <= -49.6){
+    score.number += 1;
+	    e1.x = 1000;
+	    e1.y = Math.floor(Math.random() * 250.4) + 1
+    }
+    
+        if(e2.x <= -49.6){
+	    e2.x = 1000;
+	    e2.y = Math.floor(Math.random() * 250.4) + 1
+    }
+    
+        if(e3.x <= -49.6){
+	    e3.x = 1000;
+	    e3.y = Math.floor(Math.random() * 250.4) + 1
+    }
+    
+        if(e4.x <= -49.6){
+	    e4.x = 1000;
+	    e4.y = Math.floor(Math.random() * 250.4) + 1
+    }
+    
+}}
