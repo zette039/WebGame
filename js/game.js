379fd03bac6025de998a60709af9f0a47f7f58e6
chadurvig <56233767+chadurvig@users.noreply.github.com>
2019-10-07 19:06:10 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -24,12 +24,11 @@ enemy3 = true;
 enemy4 = true;
 }
 
-var player;
 var myScore;
 function startGame() {
     myGameArea.start();
 	new component(player.x, player.y, 'images/explosion.png', 100, 80, 'image');
-    player = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
+    playeyer = new component(80, 39.6, 'images/car.png', 10, 130.2, 'image');
     e1 = new component(90, 49.6, "images/enemy.png", 1000, 20, 'image' );
     e2 = new component(90, 49.6, "images/enemy.png", 1000, 100, 'image' );
     e3 = new component(90, 49.6, "images/enemy.png", 1000, 200, 'image' );
