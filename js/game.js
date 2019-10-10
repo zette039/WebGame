d332193d4df6da57a10befc40900739dcded9d91
chadurvig <56233767+chadurvig@users.noreply.github.com>
2019-10-10 10:16:11 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -166,10 +166,11 @@ if (player.crashWith(e1)) {
     if (myGameArea.keys && myGameArea.keys[38] && player.y > 10) {player.speedY = -15; }
     if (myGameArea.keys && myGameArea.keys[40] && player.y < 250.4) {player.speedY = 15; }
     if (myGameArea.keys && myGameArea.keys[32] ) {b = true;
-                                                  bullet.x = player.x + 65;
-                                                  bullet.y = player.y + 22.3;
+                                                  
                                                   bullet.speedX = 35;}
     if (bullet.x >= 1010){
+    bullet.x = player.x + 65;
+    bullet.y = player.y + 22.3
     bullet.speedX = 0;
     b = false
     }
