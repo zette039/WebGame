0d7f2272f348726a14995c890790df8da8451880
chadurvig <56233767+chadurvig@users.noreply.github.com>
2019-10-10 23:31:44 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -30,7 +30,7 @@ var player;
 var myScore;
 function startGame() {
     myGameArea.start();
-    bullet = new component(10, 10, 'images/bullet.png', 70, 107.9, 'image');
+    bullet = new component(15, 20, 'images/bullet.png', 70, 107.9, 'image');
     player = new component(80, 44.6, 'images/car.png', 10, 130.2, 'image');
     e1 = new component(90, 54.6, "images/enemy.png", 1000, 20, 'image');
     e2 = new component(90, 54.6, "images/enemy.png", 1000, 100, 'image' );
